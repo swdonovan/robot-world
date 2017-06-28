debcf0d40b46e0b34e08fe34146dcec53106e9de
Wil Donovan <wil.donovan@gmail.com>
2017-06-28 08:29:59 -0600

    Initial commit with non built out styles

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,47 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
-  get '/robots' do
-    erb :index
+  get '/' do
+    erb :dashboard
   end
-end
 
+  get '/robots' do
+    @robots = Robot.all
+    erb :index, :layout => :'layout'
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect'/robots'
+  end
+
+  get '/robots/:id' do |id|
+    @robot = Robot.find_by_id(id.to_i)
+    erb :show
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find_by_id(params[:id].to_i)
+    erb :edit
+  end
+
+  post '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect "/robots"
+  end
+
+  post '/robots/:id/edit' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+
+  not_found do
+   erb :error
+ end
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,63 @@
 require 'sqlite3'
 
+
 class Robot
+  attr_reader :id,
+              :name,
+              :city,
+              :state,
+              :department
+
   def initialize(robot_params)
-    @database = SQLite3::Database.new('db/robot_world_development.db')
+    @id         = robot_params["id"] if robot_params["id"]
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
+    @database   = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department)
+                       VALUES (?, ?, ?, ?);", @name, @city, @state, @department
+                     )
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map {|robot| Robot.new(robot)}
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM robots
+                       WHERE id = ?;", id)
+  end
+
+  def self.find_by_id(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    # require 'pry'; binding.pry
+    Robot.new(robot)
+  end
+
+  def self.update(id, params)
+    # require'pry'; binding.pry
+    database.execute("UPDATE robots Set name = ?,
+                                        city = ?,
+                                        state = ?,
+                                        department = ?
+                                    WHERE id = ?;",
+                                    params[:name],
+                                    params[:city],
+                                    params[:state],
+                                    params[:department],
+                                    id)
+    Robot.find_by_id(id)
   end
 end
